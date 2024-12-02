eddfaa8a41286e0f2e93916f34d7c5b2153fe17b
Ralph <njugunaralph@gmail.com>
2024-12-02 12:12:34 +0300

    code challenge 3

--- challenge3.js
+++ challenge3.js
@@ -16,57 +16,35 @@ const payeDeductions = function (basicSalary) {
     } else return 'Input Basic Salary'
 }
 
-/*Function that calculates nhif deductions.
-const nhifDeductions = function (basicSalary) {
-    if (basicSalary >= 100000) {
-        return 1700;
-    } else if (basicSalary >= 90000 && basicSalary <= 99999) {
-        return 1600;
-    } else if (basicSalary >= 80000 && basicSalary <= 89999) {
-        return 1500;
-    } else if (basicSalary >= 70000 && basicSalary <= 79999) {
-        return 1400;
-    } else if (basicSalary >= 60000 && basicSalary <= 69999) {
-        return 1300;
-    } else if (basicSalary >= 50000 && basicSalary <= 59999) {
-        return 1200;
-    } else if (basicSalary >= 45000 && basicSalary <= 49999) {
-        return 1100;
-    } else if (basicSalary >= 40000 && basicSalary <= 44999) {
-        return 1000;
-    } else if (basicSalary >= 35000 && basicSalary <= 39999) {
-        return 950;
-    } else if (basicSalary >= 30000 && basicSalary <= 34999) {
-        return 900;
-    } else if (basicSalary >= 25000 && basicSalary <= 29999) {
-        return 850;
-    } else if (basicSalary >= 20000 && basicSalary <= 24999) {
-        return 750;
-    } else if (basicSalary >= 15000 && basicSalary <= 19999) {
-        return 600;
-    } else if (basicSalary >= 12000 && basicSalary <= 14999) {
-        return 500;
-    } else if (basicSalary >= 8000 && basicSalary <= 11999) {
-        return 400;
-    } else if (basicSalary >= 6000 && basicSalary <= 7999) {
-        return 300;
-    } else if (basicSalary <= 5999) {
-        return 150;
-    } else return 'Input Salary!'
-} */
-
-//Function to calculate SHIF
-const shifDeductions = basicSalary * 0.0275;
+//Function to calculate SHIF as NHIF is not valid anymore
+const shifDeductions = function (basicSalary){
+   return basicSalary * 0.0275;
+}
 
 
 //Function to calculate NSSF
 const nssfDeductionsTier1 = function (basicSalary){
    if (basicSalary <= 7000) {
     return basicSalary * 0.06;
-   } 
-}
+   } else{
+    return 0;
+   }
+};
 const nssfDeductionsTier2 = function (basicSalary) {
     if (basicSalary>= 7001 && basicSalary <= 36000) {
         return basicSalary * 0.06;
+    } else {
+        return 0; 
     }
-}
+};
+
+//test case for salary of 100000
+basicSalary = 100000
+
+const totalDeductions = payeDeductions(basicSalary) + nssfDeductionsTier2(basicSalary) + shifDeductions(basicSalary);
+
+//calculate net salary
+const netSalary = basicSalary - totalDeductions;
+
+//dispaly net salary
+console.log("Net salary:" + netSalary);
